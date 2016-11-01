b289c3ddf10c72b036a0d1c6817fdf6a8ebe140f
mitchelljfs <mitchelljfsiii@gmail.com>
2016-10-31 23:35:25 -0700

    Added bitly shorten url funtion (backend not working)

---
+++ bitly.js
@@ -0,0 +1,28 @@
+"use strict";
+
+window.BitlyShorten = (() => {
+    return {
+        init: () => {
+            var shortenButton = $("#shorten-button");
+            var longLink = $("#long-link");
+            var linkResultContainer = $(".link-result-container");
+
+            shortenButton.click(() => {
+                $.getJSON("http://api-ssl.bitly.com/v3/shorten", {
+                    access_token: "35a657c20e2b7b41fb8d72ddd04a4f7e61a640cf", 
+                    longUrl: longLink.val()
+                }).done((result) => {
+                    linkResultContainer.empty().append(
+                        result.data.map((link) => {
+                            return $("<p></p>");
+                        })
+                    );
+                });
+            });
+
+            longLink.bind("input", () => {
+                shortenButton.prop("disabled", !longLink.val());
+            });
+        }
+    };
+})();
