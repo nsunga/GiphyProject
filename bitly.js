a2938c8387a94ae5da43e974296e1141ee9aa7b3
mitchelljfs <mitchelljfsimon@gmail.com>
2016-11-01 18:39:22 -0700

    Put and example in for Shorten Link! modal

--- bitly.js
+++ bitly.js
@@ -10,7 +10,7 @@ window.BitlyShorten = (() => {
             shortenButton.click(() => {
                 $.getJSON("https://api-ssl.bitly.com/v3/shorten", {
                     access_token: "35a657c20e2b7b41fb8d72ddd04a4f7e61a640cf", 
-                    longUrl: longLink.val()
+                    longUrl: ("http://").concat(longLink.val())
                 }).done((result) => {
                     linkResultContainer.empty().append(
                         $("<div></div>").addClass("well").append(
